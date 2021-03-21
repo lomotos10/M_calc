873276dc290c4127121eabe8c0b3a48da1452e08
lomotos10 <lomotos10@gmail.com>
2021-03-21 15:49:49 +0000

    Stat refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,98 @@
 use ordered_float::OrderedFloat;
 use itertools::Itertools;
+use std::collections::HashMap;
 
+#[derive(Clone, Eq, Hash, PartialEq, Debug)]
+enum Stat {
+    MainStat,                           // 주스탯
+    MainStatPercent,                   // 주스탯퍼
+    MainStatPercentExempt,            // 스탯퍼 적용되지 않는 주스탯
+    SubStat,                            // 부스탯
+    SubStatPercent,                    // 부스탯퍼
+    SubStatPercentExempt,             // 스탯퍼 적용되지 않는 부스탯
+    Atk,                                 // 공마
+    AtkPercent,                         // 공마퍼
+    DmgPercent,                         // 뎀퍼
+    BossDmgPercent,                    // 보공
+    FinalDmgPercent,                   // 최종뎀
+    IgnoreGuardPercent,                // 방무
+    IgnoreElementalResistancePercent, // 속성내성무시
+    CritRatePercent,                   // 크확
+    CritDmgPercent,                    // 크뎀
+    WeaponConstant,                     // 무기상수
+    ClassConstant,                      // 직업상수
+    MasteryPercent,                     // 숙련도
+
+    // 추가스탯; Y136:Y141
+    ExtraDmgPercent,          // 추가뎀퍼
+    ExtraBossDmgPercent,     // 추가보공
+    ExtraFinalDmgPercent,    // 추가최종뎀
+    ExtraIgnoreGuardPercent, // 추가방무
+    ExtraCritRatePercent,    // 추가크확
+    ExtraCritDmgPercent,     // 추가크뎀
+}
 /// Members that end with `percent` are measured in units of %.  
 /// ex) dmg_percent = 50 == DMG% = 50%  
 /// Listed in X121:Y135 of Spec Simulator 0.9.4  
 /// 메용 포함  
 #[derive(Clone)]
 struct Stats {
-    main_stat: f64,                           // 주스탯
-    main_stat_percent: f64,                   // 주스탯퍼
-    main_stat_percent_exempt: f64,            // 스탯퍼 적용되지 않는 주스탯
-    sub_stat: f64,                            // 부스탯
-    sub_stat_percent: f64,                    // 부스탯퍼
-    sub_stat_percent_exempt: f64,             // 스탯퍼 적용되지 않는 부스탯
-    atk: f64,                                 // 공마
-    atk_percent: f64,                         // 공마퍼
-    dmg_percent: f64,                         // 뎀퍼
-    boss_dmg_percent: f64,                    // 보공
-    final_dmg_percent: f64,                   // 최종뎀
-    ignore_guard_percent: f64,                // 방무
-    ignore_elemental_resistance_percent: f64, // 속성내성무시
-    crit_rate_percent: f64,                   // 크확
-    crit_dmg_percent: f64,                    // 크뎀
-    weapon_constant: f64,                     // 무기상수
-    class_constant: f64,                      // 직업상수
-    mastery_percent: f64,                     // 숙련도
-
-    // 추가스탯; Y136:Y141
-    extra_dmg_percent: f64,          // 추가뎀퍼
-    extra_boss_dmg_percent: f64,     // 추가보공
-    extra_final_dmg_percent: f64,    // 추가최종뎀
-    extra_ignore_guard_percent: f64, // 추가방무
-    extra_crit_rate_percent: f64,    // 추가크확
-    extra_crit_dmg_percent: f64,     // 추가크뎀
+    inner: HashMap::<Stat, f64>
 }
 
 impl Stats {
     fn init() -> Self {
+        let mut inner = HashMap::new();
+
+        inner.insert(Stat::MainStat, 4395 as f64);
+        inner.insert(Stat::MainStatPercent, 331 as f64);
+        inner.insert(Stat::MainStatPercentExempt, 13780 as f64);
+        inner.insert(Stat::SubStat, 2014 as f64);
+        inner.insert(Stat::SubStatPercent, 103 as f64);
+        inner.insert(Stat::SubStatPercentExempt, 440 as f64);
+        inner.insert(Stat::Atk, 1954 as f64);
+        inner.insert(Stat::AtkPercent, 97 as f64);
+        inner.insert(Stat::DmgPercent, 57 as f64);
+        inner.insert(Stat::BossDmgPercent, 236 as f64);
+        inner.insert(Stat::FinalDmgPercent, 30 as f64);
+        inner.insert(Stat::IgnoreGuardPercent, 78.73660272);
+        inner.insert(Stat::IgnoreElementalResistancePercent, 5 as f64);
+        inner.insert(Stat::CritRatePercent, 70 as f64);
+        inner.insert(Stat::CritDmgPercent, 65 as f64);
+        inner.insert(Stat::WeaponConstant, 1.2);
+        inner.insert(Stat::ClassConstant, 1.0);
+        inner.insert(Stat::MasteryPercent, 90 as f64);
+
+        inner.insert(Stat::ExtraDmgPercent, 3.2);
+        inner.insert(Stat::ExtraBossDmgPercent, 0 as f64);
+        inner.insert(Stat::ExtraFinalDmgPercent, 55.94);
+        inner.insert(Stat::ExtraIgnoreGuardPercent, 42.88);
+        inner.insert(Stat::ExtraCritRatePercent, 0 as f64);
+        inner.insert(Stat::ExtraCritDmgPercent, 0 as f64);
+
         Self {
-            main_stat: 4395 as f64,
-            main_stat_percent: 331 as f64,
-            main_stat_percent_exempt: 13780 as f64,
-            sub_stat: 2014 as f64,
-            sub_stat_percent: 103 as f64,
-            sub_stat_percent_exempt: 440 as f64,
-            atk: 1954 as f64,
-            atk_percent: 97 as f64,
-            dmg_percent: 57 as f64,
-            boss_dmg_percent: 236 as f64,
-            final_dmg_percent: 30 as f64,
-            ignore_guard_percent: 78.73660272,
-            ignore_elemental_resistance_percent: 5 as f64,
-            crit_rate_percent: 70 as f64,
-            crit_dmg_percent: 65 as f64,
-            weapon_constant: 1.2,
-            class_constant: 1.0,
-            mastery_percent: 90 as f64,
-            extra_dmg_percent: 3.2,
-            extra_boss_dmg_percent: 0 as f64,
-            extra_final_dmg_percent: 55.94,
-            extra_ignore_guard_percent: 42.88,
-            extra_crit_rate_percent: 0 as f64,
-            extra_crit_dmg_percent: 0 as f64,
+            inner
         }
     }
 
+    fn get(&self, stat: Stat) -> f64{
+        *self.inner.get(&stat).unwrap()
+    }
+
     fn display_stat_atk(&self) -> f64 {
-        let main_stat_final = (self.main_stat * (1.0 + self.main_stat_percent / 100.0)
-            + self.main_stat_percent_exempt)
+        let main_stat_final = (self.get(Stat::MainStat) * (1.0 + self.get(Stat::MainStatPercent) / 100.0)
+            + self.get(Stat::MainStatPercentExempt))
             .floor();
-        let sub_stat_final = (self.sub_stat * (1.0 + self.sub_stat_percent / 100.0)
-            + self.sub_stat_percent_exempt)
+        let sub_stat_final = (self.get(Stat::SubStat) * (1.0 + self.get(Stat::SubStatPercent) / 100.0)
+            + self.get(Stat::SubStatPercentExempt))
             .floor();
-        let atk_final = (self.atk * (1.0 + self.atk_percent / 100.0)).floor();
+        let atk_final = (self.get(Stat::Atk) * (1.0 + self.get(Stat::AtkPercent) / 100.0)).floor();
 
         let stat_atk = (main_stat_final * 4.0 + sub_stat_final) * 0.01;
         let stat_atk = stat_atk * atk_final;
-        let stat_atk = stat_atk * self.weapon_constant * self.class_constant;
-        let stat_atk = stat_atk * (1.0 + self.dmg_percent / 100.0);
-        let stat_atk = stat_atk * (1.0 + self.final_dmg_percent / 100.0);
+        let stat_atk = stat_atk * self.get(Stat::WeaponConstant) * self.get(Stat::ClassConstant);
+        let stat_atk = stat_atk * (1.0 + self.get(Stat::DmgPercent) / 100.0);
+        let stat_atk = stat_atk * (1.0 + self.get(Stat::FinalDmgPercent) / 100.0);
 
         stat_atk.floor()
     }
@@ -86,31 +100,19 @@ impl Stats {
     fn get_stats_with_changes(&self, link: &LinkSkill) -> Self {
         let mut new_stats = self.clone();
         for effect in &link.effects {
-            match effect {
-                StatChange::AllStats(f) => {
-                    new_stats.main_stat += f;
-                    new_stats.sub_stat += f;
-                }
-                StatChange::AllStatsPercent(f) => {
-                    new_stats.main_stat_percent += f;
-                    new_stats.sub_stat_percent += f;
-                }
-                StatChange::Atk(f) => new_stats.atk += f,
-                StatChange::BossDmgPercent(f) => new_stats.boss_dmg_percent += f,
-                StatChange::CritDmgPercent(f) => new_stats.crit_dmg_percent += f,
-                StatChange::CritRatePercent(f) => {
-                    new_stats.crit_rate_percent += f;
-                    if new_stats.crit_rate_percent > 100.0 {
-                        new_stats.crit_rate_percent = 100.0;
+            let stat = &effect.stat;
+            let f = new_stats.inner.get_mut(&stat).unwrap();
+            match stat {
+                Stat::CritRatePercent | Stat::ExtraCritRatePercent => {
+                    *f += effect.amount;
+                    if *f > 100.0 {
+                        *f = 100.0;
                     }
                 }
-                StatChange::DmgPercent(f) => new_stats.dmg_percent += f,
-                StatChange::ExtraDmgPercent(f) => new_stats.extra_dmg_percent += f,
-                StatChange::ExtraIgnoreGuardPercent(f) => {
-                    new_stats.extra_ignore_guard_percent =
-                        add_ignore_guard_percents(vec![new_stats.extra_ignore_guard_percent, *f])
+                Stat::IgnoreGuardPercent | Stat::ExtraIgnoreGuardPercent => {
+                    *f = add_ignore_guard_percents(vec![*f, effect.amount]);
                 }
-                StatChange::IgnoreGuardPercent(f) => new_stats.ignore_guard_percent = add_ignore_guard_percents(vec![new_stats.ignore_guard_percent, *f])
+                _ => *f += effect.amount
             }
         }
 
@@ -137,56 +139,52 @@ impl CalculatorInfo {
     /// 보스상대 한줄뎀  
     /// 렙차, 아케인포스 무시
     fn boss_line_dmg(&self, stats: Stats) -> f64 {
-        let main_stat_final = (stats.main_stat * (1.0 + stats.main_stat_percent / 100.0)
-            + stats.main_stat_percent_exempt)
+        let main_stat_final = (stats.get(Stat::MainStat) * (1.0 + stats.get(Stat::MainStatPercent) / 100.0)
+            + stats.get(Stat::MainStatPercentExempt))
             .floor();
-        let sub_stat_final = (stats.sub_stat * (1.0 + stats.sub_stat_percent / 100.0)
-            + stats.sub_stat_percent_exempt)
+        let sub_stat_final = (stats.get(Stat::SubStat) * (1.0 + stats.get(Stat::SubStatPercent) / 100.0)
+            + stats.get(Stat::SubStatPercentExempt))
             .floor();
-        let atk_final = (stats.atk * (1.0 + stats.atk_percent / 100.0)).floor();
-        let dmg_percent_final = stats.dmg_percent
-            + stats.boss_dmg_percent
-            + stats.extra_dmg_percent
-            + stats.extra_boss_dmg_percent;
-        let ignore_guard_final = 1.0
-            - (1.0 - stats.ignore_guard_percent / 100.0)
-                * (1.0 - stats.extra_ignore_guard_percent / 100.0);
-        let rate = stats.crit_rate_percent + stats.extra_crit_rate_percent;
+        let atk_final = (stats.get(Stat::Atk) * (1.0 + stats.get(Stat::AtkPercent) / 100.0)).floor();
+        let dmg_percent_final = stats.get(Stat::DmgPercent)
+            + stats.get(Stat::BossDmgPercent)
+            + stats.get(Stat::ExtraDmgPercent)
+            + stats.get(Stat::ExtraBossDmgPercent);
+        let ignore_guard_percent_final = add_ignore_guard_percents(vec![stats.get(Stat::IgnoreGuardPercent), stats.get(Stat::ExtraIgnoreGuardPercent)]);
+        let rate = stats.get(Stat::CritRatePercent) + stats.get(Stat::ExtraCritRatePercent);
         let crit_rate_final = if rate > 100.0 { 1.0 } else { rate / 100.0 };
-        let crit_dmg_final = (stats.crit_dmg_percent + stats.extra_crit_dmg_percent) / 100.0;
+        let crit_dmg_final = (stats.get(Stat::CritDmgPercent) + stats.get(Stat::ExtraCritDmgPercent)) / 100.0;
 
         let mut line_dmg = (main_stat_final * 4.0 + sub_stat_final) * 0.01;
         line_dmg = line_dmg * atk_final;
-        line_dmg = line_dmg * stats.weapon_constant * stats.class_constant;
+        line_dmg = line_dmg * stats.get(Stat::WeaponConstant) * stats.get(Stat::ClassConstant);
         line_dmg = line_dmg * (1.0 + dmg_percent_final / 100.0);
         line_dmg = line_dmg
-            * (1.0 + stats.final_dmg_percent / 100.0)
-            * (1.0 + stats.extra_final_dmg_percent / 100.0);
+            * (1.0 + stats.get(Stat::FinalDmgPercent) / 100.0)
+            * (1.0 + stats.get(Stat::ExtraFinalDmgPercent) / 100.0);
         line_dmg = line_dmg
-            * (1.0 - (self.target_boss_guard_percent as f64) / 100.0 * (1.0 - ignore_guard_final));
+            * (1.0 - (self.target_boss_guard_percent as f64) / 100.0 * (1.0 - ignore_guard_percent_final / 100.0));
         line_dmg = line_dmg * (crit_rate_final * (1.35 + crit_dmg_final) + (1.0 - crit_rate_final));
-        line_dmg = line_dmg * (1.0 + stats.mastery_percent / 100.0) / 2.0;
+        line_dmg = line_dmg * (1.0 + stats.get(Stat::MasteryPercent) / 100.0) / 2.0;
         line_dmg = line_dmg
             * (1.0
                 - ((self.target_boss_elemental_resistance_percent as f64) / 100.0)
-                    * (1.0 - stats.ignore_elemental_resistance_percent / 100.0));
+                    * (1.0 - stats.get(Stat::IgnoreElementalResistancePercent) / 100.0));
 
         line_dmg
     }
 }
 
 #[derive(Debug, Clone)]
-enum StatChange {
-    DmgPercent(f64),
-    IgnoreGuardPercent(f64),
-    CritRatePercent(f64),
-    AllStats(f64),
-    Atk(f64),
-    AllStatsPercent(f64),
-    BossDmgPercent(f64),
-    CritDmgPercent(f64),
-    ExtraDmgPercent(f64),
-    ExtraIgnoreGuardPercent(f64),
+struct StatChange {
+    stat: Stat,
+    amount: f64
+}
+
+impl StatChange {
+    fn new(stat: Stat, amount: f64) -> Self {
+        Self{stat,amount}
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -209,30 +207,30 @@ fn link_skill_list() -> Vec<LinkSkill> {
         LinkSkill::new(
             "Mage",
             vec![
-                StatChange::ExtraDmgPercent(9.0),
-                StatChange::ExtraIgnoreGuardPercent(9.0),
+                StatChange::new(Stat::ExtraDmgPercent, 9.0),
+                StatChange::new(Stat::ExtraIgnoreGuardPercent, 9.0),
             ],
         ),
-        LinkSkill::new("Archer", vec![StatChange::CritRatePercent(10.0)]),
-        LinkSkill::new("Thief", vec![StatChange::ExtraDmgPercent(9.0)]),
-        LinkSkill::new("Pirate", vec![StatChange::AllStats(70.0)]),
-        LinkSkill::new("Cygnus", vec![StatChange::Atk(25.0)]),
-        LinkSkill::new("Xenon", vec![StatChange::AllStatsPercent(10.0)]),
-        LinkSkill::new("DemonSlayer", vec![StatChange::BossDmgPercent(15.0)]),
-        LinkSkill::new("DemonAvenger", vec![StatChange::DmgPercent(10.0)]),
-        LinkSkill::new("Luminous", vec![StatChange::IgnoreGuardPercent(15.0)]),
-        LinkSkill::new("Phantom", vec![StatChange::CritRatePercent(15.0)]),
-        LinkSkill::new("Cain", vec![StatChange::ExtraDmgPercent(8.5)]),
-        LinkSkill::new("Cadena", vec![StatChange::ExtraDmgPercent(12.0)]),
+        LinkSkill::new("Archer", vec![StatChange::new(Stat::CritRatePercent, 10.0)]),
+        LinkSkill::new("Thief", vec![StatChange::new(Stat::ExtraDmgPercent, 9.0)]),
+        LinkSkill::new("Pirate", vec![StatChange::new(Stat::MainStat, 70.0), StatChange::new(Stat::SubStat, 70.0)]),
+        LinkSkill::new("Cygnus", vec![StatChange::new(Stat::Atk, 25.0)]),
+        LinkSkill::new("Xenon", vec![StatChange::new(Stat::MainStatPercent, 10.0), StatChange::new(Stat::SubStatPercent, 10.0)]),
+        LinkSkill::new("DemonSlayer", vec![StatChange::new(Stat::BossDmgPercent, 15.0)]),
+        LinkSkill::new("DemonAvenger", vec![StatChange::new(Stat::DmgPercent, 10.0)]),
+        LinkSkill::new("Luminous", vec![StatChange::new(Stat::IgnoreGuardPercent,15.0)]),
+        LinkSkill::new("Phantom", vec![StatChange::new(Stat::CritRatePercent,15.0)]),
+        LinkSkill::new("Cain", vec![StatChange::new(Stat::ExtraDmgPercent, 8.5)]),
+        LinkSkill::new("Cadena", vec![StatChange::new(Stat::ExtraDmgPercent, 12.0)]),
         LinkSkill::new(
             "Adel",
-            vec![StatChange::DmgPercent(8.0), StatChange::BossDmgPercent(4.0)],
+            vec![StatChange::new(Stat::DmgPercent, 8.0), StatChange::new(Stat::BossDmgPercent, 4.0)],
         ),
-        LinkSkill::new("Ilium", vec![StatChange::DmgPercent(12.0)]),
-        LinkSkill::new("Ark", vec![StatChange::DmgPercent(11.0)]),
-        LinkSkill::new("Hoyoung", vec![StatChange::IgnoreGuardPercent(10.0)]),
-        LinkSkill::new("Zero", vec![StatChange::IgnoreGuardPercent(10.0)]),
-        LinkSkill::new("Kinesis", vec![StatChange::CritDmgPercent(4.0)]),
+        LinkSkill::new("Ilium", vec![StatChange::new(Stat::DmgPercent, 12.0)]),
+        LinkSkill::new("Ark", vec![StatChange::new(Stat::DmgPercent, 11.0)]),
+        LinkSkill::new("Hoyoung", vec![StatChange::new(Stat::IgnoreGuardPercent, 10.0)]),
+        LinkSkill::new("Zero", vec![StatChange::new(Stat::IgnoreGuardPercent, 10.0)]),
+        LinkSkill::new("Kinesis", vec![StatChange::new(Stat::CritDmgPercent, 4.0)]),
     ]
 }
 
